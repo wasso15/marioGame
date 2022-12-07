d50d7e6dcc0b39293edb9b887e384ccec1b99e9a
Guylain wasso <wassoguylain15@gmail.com>
2022-12-07 19:11:16 +0100

    set velocity and gravity to player

---
+++ index.js
@@ -0,0 +1,45 @@
+const canvas= document.querySelector('canvas');
+const c= canvas.getContext('2d'); 
+canvas.width=window.innerWidth; 
+canvas.height= window.innerHeight
+ const gravity= 0.5
+class Player{
+    constructor()
+    {
+        this.position= {
+            x:100, 
+            y:100
+        }
+        this.width=30
+        this.height=30
+        this.velocity={
+            x:0, 
+            y:0
+        }
+    }
+    draw(){
+        c.fillStyle="red"
+        c.fillRect(this.position.x, this.position.y, this.width, this.height )
+    }
+    update(){
+        this.draw()
+        this.position.y+= this.velocity.y; 
+
+        if(this.position.y + this.height + this.velocity.y <= canvas.height)
+        {
+            this.velocity.y+=gravity
+        }        
+         else this.velocity.y=0
+    }
+}
+
+const player= new Player(); 
+
+
+function animate(){
+    requestAnimationFrame(animate);
+    c.clearRect(0,0,canvas.width, canvas.height)
+    player.update();
+}
+
+animate()
