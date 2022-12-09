2524fb1da320029857e51c4595915be22fae13e0
Guylain wasso <wassoguylain15@gmail.com>
2022-12-09 20:57:29 +0100

    add movement skills to player

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const canvas= document.querySelector('canvas');
 const c= canvas.getContext('2d'); 
 canvas.width=window.innerWidth; 
 canvas.height= window.innerHeight
- const gravity= 0.5
+ const gravity=1
 class Player{
     constructor()
     {
         this.position= {
-            x:100, 
-            y:100
+            x:0 ,
+            y:0
         }
         this.width=30
         this.height=30
@@ -23,9 +23,11 @@ class Player{
     }
     update(){
         this.draw()
+        this.position.x+= this.velocity.x; 
         this.position.y+= this.velocity.y; 
+        console.log(this.position.y)
 
-        if(this.position.y + this.height + this.velocity.y <= canvas.height)
+        if(this.position.y + this.height + this.velocity.y < canvas.height)
         {
             this.velocity.y+=gravity
         }        
@@ -35,11 +37,61 @@ class Player{
 
 const player= new Player(); 
 
+const keys={
+    right:{pressed:false}, 
+    left:{pressed:false}
+}%
 
 function animate(){
     requestAnimationFrame(animate);
     c.clearRect(0,0,canvas.width, canvas.height)
     player.update();
-}
 
-animate()
+    if(keys.right.pressed)player.velocity.x=5 
+    else if(keys.left.pressed)  player.velocity.x=-5 
+    else player.velocity.x=0
+}
+ 
+animate();
+
+addEventListener('keydown', ({keyCode})=>{
+    console.log(keyCode);
+    switch (keyCode) {
+        case 38:
+            player.velocity.y-=20
+            break;
+        case 40 :
+            break;
+        case 39:
+            keys.right.pressed=true
+            break;
+        case 37:
+            keys.left.pressed=true
+            break;
+
+        default:
+            break;
+    }
+})
+
+addEventListener('keyup', ({keyCode})=>{
+    switch (keyCode) {
+        case 38:
+            break;
+        case 40 :
+            break;
+        case 39:
+            keys.right.pressed=false
+            
+            break;
+        case 37:
+            keys.left.pressed=false
+
+            break;
+
+        default:
+            break;
+    }
+})
+
+ 
